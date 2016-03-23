2c6c365241c2f91635df7c28bb40ac1777cec203
bbaiggey <1273802464@qq.com>
2016-03-24 00:21:39 +0800

    传统的线程安全问题

---
+++ src/cn/itcast/heima2/TraditionalThreadSynchronized.java
@@ -0,0 +1,89 @@
+package cn.itcast.heima2;
+/**
+ * 
+ * Description: 传统的线程安全问题
+ * Created on:  2016年3月24日 上午12:18:59 
+ * @author bbaiggey
+ */
+public class TraditionalThreadSynchronized {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new TraditionalThreadSynchronized().init();
+	}
+	
+	private void init(){
+		final Outputer outputer = new Outputer();
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while(true){
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					outputer.output("zhangxiaoxiang");
+				}
+				
+			}
+		}).start();
+		
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while(true){
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					outputer.output3("lihuoming");
+				}
+				
+			}
+		}).start();
+		
+	}
+
+	static class Outputer{
+		
+		public void output(String name){
+			int len = name.length();
+			synchronized (Outputer.class) 
+			{
+				for(int i=0;i<len;i++){
+					System.out.print(name.charAt(i));
+				}
+				System.out.println();
+			}
+		}
+		/**
+		 * 
+		 * Description: 不安全
+		 * @author bai_ge@suixingpay.com
+		 * @param  @param name
+		 * @return void
+		 * @throws
+		 */
+		public synchronized void output2(String name){
+			int len = name.length();
+			for(int i=0;i<len;i++){
+					System.out.print(name.charAt(i));
+			}
+			System.out.println();
+		}
+		
+		public static synchronized void output3(String name){
+			int len = name.length();
+			for(int i=0;i<len;i++){
+					System.out.print(name.charAt(i));
+			}
+			System.out.println();
+		}	
+	}
+}
