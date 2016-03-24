75f224d12f722df2f2e8adcb0ceda5b05743ec2f
bbaiggey <1273802464@qq.com>
2016-03-24 20:33:28 +0800

    带调度的线程池

--- src/cn/itcast/heima2/ThreadPoolTest.java
+++ src/cn/itcast/heima2/ThreadPoolTest.java
@@ -14,7 +14,7 @@ public class ThreadPoolTest {
 //		ExecutorService threadPool = Executors.newFixedThreadPool(3);		//线程池 数量是3
 //		复用可用的线程,当超过60秒没有被使用就会被移除,池子中的线程不够时,会创建新的线程
 //		ExecutorService threadPool = Executors.newCachedThreadPool();	//带缓存的线程池
-		ExecutorService threadPool = Executors.newSingleThreadExecutor();	//单线程池
+		/*ExecutorService threadPool = Executors.newSingleThreadExecutor();	//单线程池
 		for(int i=1;i<=10;i++){
 			final int task = i;
 			threadPool.execute(new Runnable(){
@@ -32,14 +32,15 @@ public class ThreadPoolTest {
 				}
 			});
 		}
-		System.out.println("all of 10 tasks have committed! ");
+		System.out.println("all of 10 tasks have committed! ");*/
 //		threadPool.shutdownNow();
 		
+//		创建一个指定数量的调度线程池				任务,第一次执行的间隔时间,多久重复一次，时间单位
 		Executors.newScheduledThreadPool(3).scheduleAtFixedRate(
 				new Runnable(){
 					@Override
 				public void run() {
-					System.out.println("bombing!");
+					System.out.println(Thread.currentThread().getName()+"执行!");
 					
 				}},
 				6,
