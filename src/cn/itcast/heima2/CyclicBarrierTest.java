537e378215425f99b49d259b8c02be69cf1b4caa
bbaiggey <1273802464@qq.com>
2016-04-01 17:04:39 +0800

    线程同步工具类		必须所有的线程都同时执行结束之后才能进行下一步操作

---
+++ src/cn/itcast/heima2/CyclicBarrierTest.java
@@ -0,0 +1,42 @@
+package cn.itcast.heima2;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/**
+ * 
+ * Description: 线程同步工具类		必须所有的线程都同时执行结束之后才能进行下一步操作
+ * Created on:  2016年4月1日 下午4:17:08 
+ * @author bbaiggey
+ */
+public class CyclicBarrierTest {
+
+	public static void main(String[] args) {
+		ExecutorService service = Executors.newCachedThreadPool();
+		final  CyclicBarrier cb = new CyclicBarrier(3);//限定要有多少的线程同时到达不然就在原地等待
+		for(int i=0;i<3;i++){
+			Runnable runnable = new Runnable(){
+					public void run(){
+					try {
+						Thread.sleep((long)(Math.random()*10000));	
+						System.out.println("线程" + Thread.currentThread().getName() + 
+								"即将到达集合地点1，当前已有" + (cb.getNumberWaiting()+1) + "个已经到达，" + (cb.getNumberWaiting()==2?"都到齐了，继续走啊":"正在等候"));						
+						cb.await();
+						
+						Thread.sleep((long)(Math.random()*10000));	
+						System.out.println("线程" + Thread.currentThread().getName() + 
+								"即将到达集合地点2，当前已有" + (cb.getNumberWaiting()+1) + "个已经到达，" + (cb.getNumberWaiting()==2?"都到齐了，继续走啊":"正在等候"));
+						cb.await();	
+						Thread.sleep((long)(Math.random()*10000));	
+						System.out.println("线程" + Thread.currentThread().getName() + 
+								"即将到达集合地点3，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting()==2?"都到齐了，继续走啊":"正在等候"));						
+						cb.await();						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}				
+				}
+			};
+			service.execute(runnable);
+		}
+		service.shutdown();
+	}
+}
